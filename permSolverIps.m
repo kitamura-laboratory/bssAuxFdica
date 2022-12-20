968f254d0f3cc7e1c33c8ae5f6bb0eabfecee9bd
Daichi Kitamura <d-kitamura@ieee.org>
2022-12-20 21:52:48 +0900

    support arbitrary number of sources

--- permSolverIps.m
+++ permSolverIps.m
@@ -23,47 +23,23 @@ end
 if isreal(mix); error("'mix' must be complex-valued spectrograms.\n"); end
 if isreal(src); error("'src' must be complex-valued spectrograms.\n"); end
 if ~isequal(size(mix), size(src)); error("Sizes of 'mix' and 'src' must be equal.\n"); end
-if nSrc >= 4; error("permSolverIps for nSrc >= 4 has not been implemented yet.\n"); end
 
 % Align estimated spectrogram using oracle source spectrogram
 est = zeros(nFreq, nTime, nSrc);
 perm = zeros(nFreq, nSrc);
-if nSrc == 2
-    for iFreq = 1:nFreq
-        err(1) = sum(abs(mix(iFreq, :, 1) - src(iFreq, :, 1)).^2 + abs(mix(iFreq, :, 2) - src(iFreq, :, 2)).^2, "all");
-        err(2) = sum(abs(mix(iFreq, :, 2) - src(iFreq, :, 1)).^2 + abs(mix(iFreq, :, 1) - src(iFreq, :, 2)).^2, "all");
-        if err(1) <= err(2)
-            perm(iFreq, :) = [1, 2];
-        else
-            perm(iFreq, :) = [2, 1];
+permAll = perms(1:nSrc);
+nPerm = size(permAll, 1); % = factorial(nSrc)
+for iFreq = 1:nFreq
+    for iPerm = 1:nPerm
+        err(iPerm) = 0;
+        for iSrc = 1:nSrc
+            mixInd = permAll(iPerm, iSrc);
+            err(iPerm) = err(iPerm) + sum(abs(mix(iFreq, :, mixInd) - src(iFreq, :, iSrc)).^2, "all");
         end
-        est(iFreq, :, :) = mix(iFreq, :, perm(iFreq,:));
     end
-else
-    for iFreq = 1:nFreq
-        err(1) = sum(abs(mix(iFreq, :, 1) - src(iFreq, :, 1)).^2 + abs(mix(iFreq, :, 2) - src(iFreq, :, 2)).^2 + abs(mix(iFreq, :, 3) - src(iFreq, :, 3)).^2, "all");
-        err(2) = sum(abs(mix(iFreq, :, 1) - src(iFreq, :, 1)).^2 + abs(mix(iFreq, :, 3) - src(iFreq, :, 2)).^2 + abs(mix(iFreq, :, 2) - src(iFreq, :, 3)).^2, "all");
-        err(3) = sum(abs(mix(iFreq, :, 2) - src(iFreq, :, 1)).^2 + abs(mix(iFreq, :, 1) - src(iFreq, :, 2)).^2 + abs(mix(iFreq, :, 3) - src(iFreq, :, 3)).^2, "all");
-        err(4) = sum(abs(mix(iFreq, :, 2) - src(iFreq, :, 1)).^2 + abs(mix(iFreq, :, 3) - src(iFreq, :, 2)).^2 + abs(mix(iFreq, :, 1) - src(iFreq, :, 3)).^2, "all");
-        err(5) = sum(abs(mix(iFreq, :, 3) - src(iFreq, :, 1)).^2 + abs(mix(iFreq, :, 1) - src(iFreq, :, 2)).^2 + abs(mix(iFreq, :, 2) - src(iFreq, :, 3)).^2, "all");
-        err(6) = sum(abs(mix(iFreq, :, 3) - src(iFreq, :, 1)).^2 + abs(mix(iFreq, :, 2) - src(iFreq, :, 2)).^2 + abs(mix(iFreq, :, 1) - src(iFreq, :, 3)).^2, "all");
-        [~, ind] = min(err);
-        switch(ind)
-            case 1
-                perm(iFreq, :) = [1, 2, 3];
-            case 2
-                perm(iFreq, :) = [1, 3, 2];
-            case 3
-                perm(iFreq, :) = [2, 1, 3];
-            case 4
-                perm(iFreq, :) = [2, 1, 3];
-            case 5
-                perm(iFreq, :) = [3, 1, 2];
-            case 6
-                perm(iFreq, :) = [3, 2, 1];
-        end
-        est(iFreq, :, :) = mix(iFreq, :, perm(iFreq,:));
-    end
+    [~, ind] = min(err);
+    perm(iFreq, :) = permAll(ind, :);
+    est(iFreq, :, :) = mix(iFreq, :, perm(iFreq,:));
 end
 
 fprintf("Permutation solver (IPS) done.\n");
